90de07e0e022327d90a840e1e4e9443566413789
seomintaek <mtseo1@snu.ac.kr>
2025-12-12 08:47:51 +0900

    Update Vite configuration to include 'practice.html' in build inputs

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-// vite.config.js
-import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
 export default defineConfig({
-    build: {
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, 'index.html'),
-                sensors: resolve(__dirname, 'sencors.html'),
-            },
-        },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        sensors: resolve(__dirname, 'sencors.html'),
+        practice: resolve(__dirname, 'practice.html'),
+      },
     },
-});
+  },
+})
